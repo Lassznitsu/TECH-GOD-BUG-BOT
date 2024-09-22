32ebd96b37eebe0899e97f2b452e7655d0dda4d5
Lassznitsu <diabylassana131@gmail.com>
2024-09-22 09:06:02 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "910000000000"
-global.ownername = "🐛Tech God"
+global.ownernomer = "447448237803"
+global.ownername = "Lass🪽🦇♟️"
 global.ytname = "YT: Tech-God"
 global.socialm = "GitHub: techgod143"
-global.location = "India, Up, Rampur"
+global.location = "Angleterre"
 
-global.ownernumber = '917466008456'  //creator number
-global.ownername = '🐛Tech god' //owner name
+global.ownernumber = '447448237803'  //creator number
+global.ownername = 'Lass🪽🦇♟️' //owner name
 global.botname = 'Tech God ᵇᵘᵍ ᵇᵒᵗ' //name of the bot
 
 //sticker details
